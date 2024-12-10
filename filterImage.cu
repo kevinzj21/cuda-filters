6e0fe4ab9b3f9d8f8d7a4c5596f774bdb4d368a8
Kevin Zhao <kevin.zhaoj21@gmail.com>
2024-12-10 00:49:35 -0500

    Boilerplate and stb image library

---
+++ filterImage.cu
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <iostream>
+#define STB_IMAGE_IMPLEMENTATION
+#include <lib/stb_image.h>
+
+int main(int argc, char const *argv[])
+{
+    std::cout<<"Load image"<<std::endl;
+
+    return 0;
+}
